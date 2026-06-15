e0509511085d1b6aa7c56133913159fbbd64f214
Camila Aballay <aballaycamila4@gmail.com>
2026-06-15 10:42:41 -0300

    Primer commit

---
+++ js/inv.js
@@ -0,0 +1,29 @@
+
+    function crearConfetti() {
+      const colores = ['#ff4fa3', '#ffd166', '#ffffff', '#ff85c2', '#c77dff'];
+
+      for (let i = 0; i < 90; i++) {
+        const confetti = document.createElement('div');
+        confetti.classList.add('confetti');
+
+        confetti.style.left = Math.random() * 100 + 'vw';
+        confetti.style.backgroundColor = colores[Math.floor(Math.random() * colores.length)];
+        confetti.style.animationDuration = (4 + Math.random() * 4) + 's';
+        confetti.style.animationDelay = Math.random() * 5 + 's';
+        confetti.style.width = (8 + Math.random() * 8) + 'px';
+        confetti.style.height = (8 + Math.random() * 8) + 'px';
+
+        document.body.appendChild(confetti);
+      }
+    }
+const botonCerrar = document.getElementById("cerrarDialogo");
+const dinoBox = document.getElementById("dinoBox");
+
+if (botonCerrar && dinoBox) {
+  botonCerrar.addEventListener("click", () => {
+    dinoBox.style.display = "none";
+  });
+}
+  
+    crearConfetti();
+
